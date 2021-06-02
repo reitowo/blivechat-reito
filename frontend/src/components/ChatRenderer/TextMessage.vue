fde52ae724212d8d9535164ba0bb56f04073a62b
DoodleBear <yangmufeng233@gmail.com>
2021-06-02 18:43:07 +0800

    style: 代码格式，改用v-for + v-if来渲染content，替换v-html，支持会员表情包,仅特定舰队等级才可以使用

--- frontend/src/components/ChatRenderer/TextMessage.vue
+++ frontend/src/components/ChatRenderer/TextMessage.vue
@@ -18,10 +18,11 @@
         </span>
       </yt-live-chat-author-chip>
       <!-- 用户自定的弹幕图片 -->
-      <div id="message-photo" class="style-scope yt-live-chat-text-message-renderer" v-html="contents" >
-        
-      </div>
-     
+      <span v-for="(item, index) in contents" :key="index" >
+        <img v-if="item.type == 'image'" :height="item.height" width="auto" :src="`/static/${item.content}`" />
+        <span v-else>{{item.content}}</span>
+      </span>
+
       <span id="message" class="style-scope yt-live-chat-text-message-renderer">{{
         }}<el-badge :value="repeated" :max="99" v-show="repeated > 1" class="style-scope yt-live-chat-text-message-renderer"
           :style="{'--repeated-mark-color': repeatedMarkColor}"
@@ -36,72 +37,27 @@ import ImgShadow from './ImgShadow.vue'
 import AuthorBadge from './AuthorBadge.vue'
 import * as constants from './constants'
 import * as utils from '@/utils'
+import axios from 'axios'
 
 
-var face = {
-    // 由 danmu_pic.json 自动生成，下面为生成案例
-    // '[希望之花]': '</span>' + '<img height="128" width="auto" src="/static/希望之花.gif" />' + '<span>',
-}
-//* 这样怎么打都不会错了吧(), 可以【】也可以【]，各种奇妙组合，但】【不行，(话说这么打的真的人才)
-// 中文单双引号会交替出现所以做了容错
-var reg = /(“|”|"|'|‘|’|\[|【|{).+?(”|“|"|'|’|‘|\]|】|})/g
-
-// 使用正则表达式把原本的 content 中的[xx]换为标签元素
-function regexReplace(str) {
-    
-  // 如果存在则替换
-  str = str.replace(reg,function(keyword){ 
-    // 下方可选, 主要是将 [  钱  ], 等前后空白的情况替换为 [钱] 的容错机制
-    var new_str = keyword.substring(1, keyword.length - 1);
-    new_str = new_str.trim();
-    if(face[new_str])
-      return face[new_str]
-    else {
-      return keyword
-    }
-  })
-  return str
-}
-
-function convertTextToImg(from_str) {
-    // 把原本的 content 中的 [xx]换为标签元素
-    var to_str = regexReplace(from_str);
-    return '<span id="message" class="style-scope yt-live-chat-text-message-renderer">'+ to_str +'</span>'
-}
-
-var json
-
-// 在页面刷新缓存时, 读取用户danmu_pic.json, 并建立表情包库
-window.onload = function () {
-  var url = "/danmu_pic.json" /*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
-  var request = new XMLHttpRequest();
-  request.open("get", url);/*设置请求方法与路径*/
-  request.send(null);/*不发送数据到服务器*/
-  request.onload = function () {/*XHR对象获取到返回信息后执行*/
-      if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
-          json = JSON.parse(request.responseText);
-          for(var i=0;i<json.length;i++){
-              //* 从 json 读取数据, 创建 keyword 和 image 的对应关系
-              var _from = json[i].keyword;
-              var _to = `</span><img height="${json[i].height}" width="auto" src="/static/${json[i].image}" /><span>` 
-              face[_from] = _to;
-              console.log(json[i].keyword);
-          }
-          console.log(json);
-      }
-  }
-}
 
 // HSL
 const REPEATED_MARK_COLOR_START = [210, 100.0, 62.5]
 const REPEATED_MARK_COLOR_END = [360, 87.3, 69.2]
 
+const split_regex = /(“|”|【|】)/g
+// 在页面刷新缓存时, 读取用户danmu_pic.json, 并建立表情包库
+window.onload = function () {
+  axios.get('/danmu_pic.json')
+  .then((res) => {
+    json = res.data
+  })
+}
+let json
+
 export default {
   name: 'TextMessage',
   data() {
-    return {
-      danmu_pic:json
-    }
   },
   components: {
     ImgShadow,
@@ -118,13 +74,42 @@ export default {
   },
   computed: {
     contents() {
-      return convertTextToImg(this.content)
-    },
-    filter_danmu_pic() {
-      // 判断是否显示图片
-      return this.danmu_pic.filter((pic) => {
-        return pic.keyword == this.content
-      })
+        // 使用正则表达式把原本的 content 中的[xx]换为渲染资讯
+        let str_arr = this.content.split(split_regex) // 切分原字段
+        let render_arr = [] // 存切分后转换出的渲染data
+        let index = 0
+        // * 解析 split 出来的 string array
+        for(let i = 0; i < str_arr.length; i++) {
+          if((str_arr[i] == '“' || str_arr[i] == '【') && i + 1 < str_arr.length) {
+            if(!json[str_arr[i + 1]]) {
+              render_arr[index] = {
+                type: 'text',
+                content : str_arr[i]
+              } 
+              index++
+              continue
+            } // 不存在关键词
+            else {
+              let _face = json[str_arr[i + 1]]
+              if((this.privilegeType > 0 && this.privilegeType < _face.rank) || _face.rank==0) {
+                render_arr[index] = {
+                  type: 'image',
+                  content : _face.image,
+                  height : _face.height,
+                }
+                index++
+                i+=2  // 跳过后2个判断
+                continue
+              } // 判断是否有权限使用表情包
+            } // 存在关键词
+          } // 判断触发关键字
+          render_arr[index] = {
+            type: 'text',
+            content : str_arr[i]
+          } // 非触发关键字or无权限使用表情包时渲染文字
+          index++
+        }
+      return render_arr
     },
     timeText() {
       return utils.getTimeTextHourMin(this.time)
