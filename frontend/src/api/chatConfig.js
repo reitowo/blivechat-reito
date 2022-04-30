80bd5f6eff219f04ed6bfefea71ac09931699b5a
DoodleBears <yangmufeng233@gmail.com>
2022-05-01 02:45:48 +0800

    fix: 增加新的默认表情包

--- frontend/src/api/chatConfig.js
+++ frontend/src/api/chatConfig.js
@@ -44,220 +44,255 @@ export const DEFAULT_CONFIG = {
 
   emoticons: [
     {
+      "keyword": "猫猫九轴",
+      "level": 0,
+      "align": "block",
+      "height": 120,
+      "url": "\\emoticons\\angleXY.gif"
+    },
+    {
+      "keyword": "九轴",
+      "level": 0,
+      "align": "block",
+      "height": 120,
+      "url": "\\emoticons\\na_o_mi_angleXY.gif"
+    },
+    {
+      "keyword": "好",
+      "level": 0,
+      "align": "block",
+      "height": 64,
+      "url": "\\emoticons\\hao.png"
+    },
+    {
+      "keyword": "可以涩涩",
+      "level": 0,
+      "align": "block",
+      "height": 140,
+      "url": "\\emoticons\\h_ok.png"
+    },
+    {
+      "keyword": "大笑死",
+      "level": 0,
+      "align": "block",
+      "height": 100,
+      "url": "\\emoticons\\wa_ra_u.gif"
+    },
+    {
       "keyword": "草",
       "level": 0,
       "align": "inline",
-      "height": 16,
+      "height": 20,
       "url": "\\emoticons\\kusa.jpg"
     },
     {
       "keyword": "吓我一跳",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\bi_ku_ri_si_da.gif"
     },
     {
       "keyword": "哭了",
       "level": 0,
       "align": "inline",
-      "height": 16,
+      "height": 20,
       "url": "\\emoticons\\cry.png"
     },
     {
       "keyword": "希望人没事",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\da_i_jo_u_bu.jpg"
     },
     {
       "keyword": "不中",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\da_me.jpg"
     },
     {
       "keyword": "摸不着头脑",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\do_u_i_u_ko_to.jpg"
     },
     {
       "keyword": "喝茶",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\drink_tea.gif"
     },
     {
       "keyword": "饮茶先",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\drink_tea_yeah.jpg"
     },
     {
       "keyword": "我看不懂",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\e_gu_i.jpg"
     },
     {
       "keyword": "emo",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\emo.jpg"
     },
     {
       "keyword": "做猪最重要的是开心",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\happy_is_important.gif"
     },
     {
       "keyword": "地铁老人手机",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\i_mi_wa_ka_ran.jpg"
     },
     {
       "keyword": "杀了我",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\kill_me.jpg"
     },
     {
       "keyword": "钱",
       "level": 0,
       "align": "inline",
-      "height": 16,
+      "height": 20,
       "url": "\\emoticons\\money.png"
     },
     {
       "keyword": "睡了",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\ne_ru.gif"
     },
     {
       "keyword": "yabe",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\yabe.jpg"
     },
     {
       "keyword": "不忍直视",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\yabe.jpg"
     },
     {
       "keyword": "笑死",
       "level": 0,
       "align": "inline",
-      "height": 16,
+      "height": 20,
       "url": "\\emoticons\\wa_ra_u.gif"
     },
     {
       "keyword": "二次元是大家的家",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\yamero.jpg"
     },
     {
       "keyword": "不够色",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\not_h.jpg"
     },
     {
       "keyword": "晚安",
       "level": 0,
       "align": "inline",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\o_ya_su_mi.jpg"
     },
     {
       "keyword": "中嘞",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\ojbk.jpg"
     },
     {
       "keyword": "明白",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\wa_ka_tta.png"
     },
     {
       "keyword": "铁咩",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\tie_mie.jpg"
     },
     {
       "keyword": "好耶",
       "level": 0,
       "align": "block",
-      "height": 80,
-      "url": "\\emoticons\\yo_sha.jpg"
+      "height": 100,
+      "url": "\\emoticons\\hao_ye.jpg"
     },
     {
       "keyword": "勇！",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\yu_u_ki.jpg"
     },
     {
       "keyword": "二次元是不会背叛你的",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\ni_ji_gen_never_die_1.jpg"
     },
     {
       "keyword": "呜呜呜",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\hai_sifu_cry.png"
     },
     {
       "keyword": "可以H",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\h_ok.png"
     },
     {
       "keyword": "椅师傅",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\chair_sifu.png"
     },
     {
       "keyword": "小椅师傅",
       "level": 0,
       "align": "inline",
-      "height": 16,
+      "height": 20,
       "url": "\\emoticons\\chair_sifu.png"
     },
     {
@@ -271,7 +306,7 @@ export const DEFAULT_CONFIG = {
       "keyword": "海式",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\hai_sifu.png"
     },
     {
@@ -285,7 +320,7 @@ export const DEFAULT_CONFIG = {
       "keyword": "尚未",
       "level": 0,
       "align": "block",
-      "height": 80,
+      "height": 100,
       "url": "\\emoticons\\na_o_mi.png"
     }
   ] // [{ keyword: '', url: '' }, ...]
