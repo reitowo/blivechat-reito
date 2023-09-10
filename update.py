6d23f8f9ff1da9a9f32e20fdfb58f66fe514b433
John Smith <xfgryujk@126.com>
2023-09-10 10:40:19 +0800

    更新版本号v1.8.0

--- update.py
+++ update.py
@@ -5,8 +5,8 @@ import aiohttp
 
 import utils.request
 
-VERSION = 'v1.7.0'
-DOODLEBEAR_VERSION = 'v1.7.2-230916'
+VERSION = 'v1.8.0'
+DOODLEBEAR_VERSION = 'v1.8.0-230917'
 
 def check_update():
     asyncio.get_running_loop().create_task(_do_check_update())
